48ee54be356eba95c0c476d2b368feb31e110d7a
TheLeastRemove <TheLeastRemove@outlook.com>
2022-09-21 23:51:58 +0800

    二分搜索-构建

---
+++ Algorithm/binarySearch.h
@@ -0,0 +1,27 @@
+/**
+  * @file     	binarySearch.h
+  * @author   	TheLeastRemove
+  * @email   	TheLeastRemove@outlook.com
+  * @version	V0.1
+  * @date    	22/9/21
+  * @brief      
+  * @detail		¶þ·ÖËÑË÷
+  * @attention
+  *  Not completed                                                \n
+  * <h2><center>&copy;TheLeastRemove All Rights Reserved.</center></h2>
+  */
+//
+// Created by TheLeastRemove on 22/9/21.
+//
+
+#ifndef STUDYAALGORITHM_BINARYSEARCH_H
+#define STUDYAALGORITHM_BINARYSEARCH_H
+
+
+class binarySearch
+{
+
+};
+
+
+#endif //STUDYAALGORITHM_BINARYSEARCH_H
