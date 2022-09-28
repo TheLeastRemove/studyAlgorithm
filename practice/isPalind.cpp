f7592a024edb5b1979f841a6c82362538f01b7a3
TheLeastRemove <TheLeastRemove@outlook.com>
2022-09-28 13:08:16 +0800

    回文序列_舍友作业。

---
+++ practice/isPalind.cpp
@@ -0,0 +1,37 @@
+/**
+  * @file     	isPalind.cpp
+  * @author   	TheLeastRemove
+  * @email   	TheLeastRemove@outlook.com
+  * @version	V0.1
+  * @date    	22/9/27
+  * @brief      
+  * @detail		判断回文序列，以@分割，舍友作业。
+  * @attention
+  *  Not completed                                                \n
+  * <h2><center>&copy;TheLeastRemove All Rights Reserved.</center></h2>
+  */
+
+#include <iostream>
+#include <stack>
+void isPalind(){
+    char inData[100];
+    std::cin>>inData;
+    std::stack<char>halfData;
+    int i(0);
+    for (i = 0; i < 100; ++i){
+        if(inData[i]=='@'){
+            halfData.push(inData[i]);
+        }else{
+            break;
+        }
+    }
+    bool flag(true);
+    for (int j=1;j<i;j++)
+    {
+        if(inData[i+j]!=halfData.top()){
+            flag = false;
+        }
+        halfData.pop();
+    }
+    std::cout<<flag;
+}
